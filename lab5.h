07f226594fee319e9371ec8009ef9768e8c062ba
Roman Matyushkin <144627459+amroman1337@users.noreply.github.com>
2025-11-26 23:08:06 +0300

    Delete lab5.h

--- lab5.h
+++
@@ -1,8 +0,0 @@
-#pragma once // говорит компилятору что данный файл должен быть включен только один раз
-
-#include <iostream>
-#include <cmath>
-#include <utility>
-
-bool isSquare(int num);
-std::pair<int, int> closest_pair_tonum(int upper_limit);
